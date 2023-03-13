6c3cada63fb57245a7011e926096b7cf4acca627
Quan2311 <115069794+Quan2311@users.noreply.github.com>
2023-03-13 18:17:46 +0700

    Add files via upload

---
+++ Central Zoo/donate/donate.js
@@ -0,0 +1,14 @@
+const form = document.getElementById("donation-form");
+const resultContainer = document.getElementById("result-container");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+
+  const data = {
+    name: formData.get("name"),
+    email: formData.get("email"),
+    phone: formData.get("phone"),
+    account number: formData.get("accoun number"),
+    amount: formData.get("
